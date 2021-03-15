459d7f8f839d1f1da7bfa783c618b2d7cee6482e
Zahra Abrahamson <calisunshine@tuta.io>
2021-03-15 14:43:01 -0400

    finish bin2sc.py

--- bin2sc.py
+++ bin2sc.py
@@ -15,12 +15,42 @@ def main(binfile):
 
     proc = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
     out, err = proc.communicate()
-    print(out)
-    
+    out = out.decode("utf-8")
+
+    opcodes = []
+    for line in out.split("\n"):
+        # if line has as line number starting it, it is code
+        match = re.match(r"\s+[0-9]+:", line)
+        if match:
+            opcodes_raw = line.split(":")[1].strip()
+            opcode = ""
+            for c in opcodes_raw:
+                if c in set("0123456789abcdef"):
+                    opcode += c
+                elif c == " " and opcode != "":
+                    opcodes.append(opcode)
+                    opcode = ""
+                else:
+                    break
+
+    # generate output
+    print("\"", end = "")
+    for i, op in enumerate(opcodes):
+        print("\\x{}".format(op), end="")
+        # add a closing quote after 16 characters and start a new line
+        if i % 16 == 15:
+            print("\"")
+            print("\"", end = "")
+    # add a closing quote if it wasn't already added in the for-loop
+    if len(opcodes) % 16 != 15:
+        print("\"")
+
+    print()
+    print("[i] shellcode length: {}".format(len(opcodes)))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("usage: {} file.o\n".format(sys.argv[0]))
         sys.exit(0)
 
-    main(sys.argv[1]))
+    main(sys.argv[1])
